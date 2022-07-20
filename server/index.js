d18acbe2f90554891874c5b599dcc96f1c80e689
emma <yinhongduan@gmail.com>
2022-07-20 11:07:48 -0700

    init25

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.get("/", (req, res) => {
-  res.sendFile(path.join(___dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 // check the value of port. If it does not have one, we use 4005 by default.
 const port = process.env.PORT || 4005;
